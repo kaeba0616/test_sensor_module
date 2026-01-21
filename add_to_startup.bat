8e8462bc1cc8aea1332482e4b0f74472d09bb828
hidi <hidi@qm391-0322.cafe24.com>
2026-01-21 21:09:35 +0900

    modify sensorModule position

--- add_to_startup.bat
+++ add_to_startup.bat
@@ -1,14 +1,13 @@
 @echo off
 chcp 65001 >nul
-echo Windows 시작 시 자동 실행 등록...
+echo 시작 프로그램 바로가기 생성...
 
 set INSTALL_DIR=%~dp0
 set SHORTCUT_NAME=SensorModule.lnk
-set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 
-:: VBScript로 바로가기 생성
+:: VBScript로 바로가기 생성 (현재 폴더에)
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%STARTUP_FOLDER%\%SHORTCUT_NAME%" >> CreateShortcut.vbs
+echo sLinkFile = "%INSTALL_DIR%%SHORTCUT_NAME%" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%INSTALL_DIR%start_background.bat" >> CreateShortcut.vbs
 echo oLink.WorkingDirectory = "%INSTALL_DIR%" >> CreateShortcut.vbs
@@ -19,8 +18,11 @@ cscript //nologo CreateShortcut.vbs
 del CreateShortcut.vbs
 
 echo.
-echo 시작 프로그램에 등록되었습니다.
-echo 위치: %STARTUP_FOLDER%\%SHORTCUT_NAME%
+echo 바로가기가 생성되었습니다: %INSTALL_DIR%%SHORTCUT_NAME%
 echo.
-echo 제거하려면 해당 파일을 삭제하세요.
+echo 시작 프로그램으로 등록하려면:
+echo   1. Win + R 키
+echo   2. shell:startup 입력 후 Enter
+echo   3. %SHORTCUT_NAME% 파일을 해당 폴더로 이동
+echo.
 pause
